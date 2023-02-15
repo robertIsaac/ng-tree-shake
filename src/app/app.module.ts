3efc6321ded6d19b3318c0d9cd535d768af9fe61
robertIsaac <dev.robert.isaac@gmail.com>
2023-02-15 11:12:50 +0200

    use ngx-markdown and provide default SECURITY_CONTEXT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SECURITY_CONTEXT } from 'ngx-markdown';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SECURITY_CONTEXT,
+      useValue: SecurityContext.HTML,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
